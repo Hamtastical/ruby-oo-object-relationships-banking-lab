6ef9688f3b033716f91f9f30c4899d6b60b58ea2
meili <meiliham1@gmail.com>
2021-03-07 15:49:58 -0500

    commit tree 1aeb5e12388667c0d5e1bfce0b2c327cd1a8756a

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,48 @@
+require "pry"
+
 class BankAccount
+
+    attr_accessor :balance, :status
+    attr_reader :name
+
+    def initialize(name) #initialize with a name
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def name
+        @name
+    end
+
+    def deposit(amount) # Me.deposit(1000) = bal  is 2000
+
+        self.balance += amount #balance amount gets deposited
+        
+    end
+
+    def display_balance #display balance of self
+
+        "Your balance is $#{self.balance}."
+    end
+
+
+
+
+    def valid? #valid? will ask if it is true or not (conditional used)
+
+         if self.status == "open" && self.balance > 0  #status is open and balance is greater than 0, it is true
+            true
+
+         else
+            false
+         end
+    end
+
+    def close_account
+
+        self.status = "closed" #status of the account is closed
+
+    end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,54 @@
+require "pry"
+
 class Transfer
-  # your code here
+
+  attr_accessor :status
+  attr_reader :amount, :sender, :receiver
+
+  def initialize(sender, receiver, amount) #it is sender, reciever and the amount ina rguments because those are the ones that will be entered for future methods
+    @sender = sender
+    @receiver = receiver
+    @status = "pending" #this method can be changed later
+    @amount = amount
+  end
+
+  def amount
+    @amount
+  end
+
+  def valid?
+
+    #can call upon method itself to check
+      if self.sender.valid? == true  && self.receiver.valid? == true #if the sender and reciever is valid and == true, then true
+        true
+
+      else #else it is false
+        false
+    end
+  end
+
+  def execute_transaction #amanda.transfer(200) = amanda.balance = 950
+    if 
+    self.status == "pending" && self.sender.valid? == true && self.sender.balance >= self.amount #to make a transaction happen once, we can use if/else (if it can transfer, then transfer, if not then reject)
+    self.sender.balance -= self.amount #the sender will minus from the account
+    self.receiver.balance += self.amount #the reciever willa dd to the account (money recieved)
+    self.status = "complete" #status is complete
+
+    else
+      self.status == "pending" && self.sender.valid? == false || self.sender.balance < self.amount
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+      
+    end
+  end
+
+def reverse_transfer
+  if execute_transaction && self.status = "complete"
+  self.receiver.balance -= self.amount
+  self.sender.balance += self.amount
+  self.status = "reversed"
+  end
+
+end
+  
 end
